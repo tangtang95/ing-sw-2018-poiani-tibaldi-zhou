fffa99cda29cf60e696d29afc85196c8b09bfc1c
riccardopoiani <37558061+riccardopoiani@users.noreply.github.com>
2018-04-21 23:36:43 +0200

    Add javadocs for PrivateObjectiveCard

--- src/main/java/org/poianitibaldizhou/sagrada/game/model/cards/PrivateObjectiveCard.java
+++ src/main/java/org/poianitibaldizhou/sagrada/game/model/cards/PrivateObjectiveCard.java
@@ -13,21 +13,42 @@ public class PrivateObjectiveCard extends Card implements IScore {
     /**
      * Constructor.
      * Creates a PrivateObjectiveCard with a certain name, description and a ColorConstraint.
+     * Each PrivateObjectiveCard defines rules on which calculate additional score based
+     * on a certain color.
      *
-     * @param name
-     * @param description
-     * @param colorConstraint
+     * @param name card's name
+     * @param description card's description
+     * @param colorConstraint card's color on which calculate additional score
      */
     public PrivateObjectiveCard(String name, String description, ColorConstraint colorConstraint) {
         super(name, description);
         this.colorConstraint = colorConstraint;
     }
 
-
+    /**
+     * Constructor.
+     * Creates a PrivateObjectiveCard with a certain name, description and a ColorConstraint.
+     * Each PrivateObjectiveCard defines a rule based on a certain color on which calculate
+     * additional score.
+     * Example:
+     * "Sum the values of the green dices"
+     *
+     * @param name card's name
+     * @param description card's description
+     * @param color card's color on which calculate additional score
+     */
     public PrivateObjectiveCard(String name, String description, Color color) {
         this(name, description, new ColorConstraint(color));
     }
 
+    /**
+     * Calculates score on a rule that would be like "Sum the values of the green dices".
+     * This operation is done looking at a SchemaCard.
+     *
+     * @param schema SchemaCard on which to perform the operation
+     * @return the score calculated using the color of the PrivateObjectiveCard and the dices
+     *         present on schema.
+     */
     @Override
     public int getScore(SchemaCard schema) {
         int score = 0;
