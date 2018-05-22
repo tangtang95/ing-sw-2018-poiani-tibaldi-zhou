3ccd0017b39f657fd30092be7eab741e1bd0ca61
riccardopoiani <37558061+riccardopoiani@users.noreply.github.com>
2018-05-22 15:48:06 +0200

    Add javadoc to DrawableCollection and DraftPool observers

--- src/main/java/org/poianitibaldizhou/sagrada/game/model/observers/IDrawableCollectionObserver.java
+++ src/main/java/org/poianitibaldizhou/sagrada/game/model/observers/IDrawableCollectionObserver.java
@@ -3,7 +3,24 @@ package org.poianitibaldizhou.sagrada.game.model.observers;
 import java.util.List;
 
 public interface IDrawableCollectionObserver<T> {
+    /**
+     * Notify that an element has been added to the DrawableCollection
+     *
+     * @param elem element added
+     */
     void onElementAdd(T elem);
+
+    /**
+     * Notify that a list of elements has been added to the DrawableCollection
+     *
+     * @param elem list of elements added
+     */
     void onElementsAdd(List<T> elem);
+
+    /**
+     * Notify that an element has been removed from the DrawableCollection
+     *
+     * @param elem element removed
+     */
     void onElementDraw(T elem);
 }
